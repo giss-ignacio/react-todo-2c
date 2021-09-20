281c483185ddf621a7cd150cbc64e22be9684fa7
Ignacio Giss <rgiss@mulesoft.com>
2021-09-19 22:44:28 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Tareas from './Tareas'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        tareas:[]
+    };
+  }
+
+  addTodo() {
+    const tarea = {
+      titulo : prompt ("Tarea a realizar"),
+      checked:false
+    }
+
+    const newArray = Array.from(this.state.tareas)
+    newArray.push(tarea)
+    this.setState({tareas:newArray})
+  }
+
+
+  render() {
+    return (
+      <div>
+      <h1 class="center title">TODO App</h1>
+      <div class="flow-right controls">
+
+        <span>Item count: <span id="item-count">0</span></span>
+        <span>Unchecked count: <span id="unchecked-count">0</span></span>
+      </div>
+      <button class="button center" onClick={this.addTodo.bind(this)}>Agregar Tarea</button>
+
+
+        <Tareas tareas={this.state.tareas}/>
+
+      </div>
+    );
+  }
 }
 
 export default App;
